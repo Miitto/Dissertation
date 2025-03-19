c2ed8bae4641f85d3c35996c252a3982c20557c1
Miitto <wpbickerton@gmail.com>
2025-03-19 18:59:58 +0000

    feat: Default values for lighting uniforms

--- engine/src/shaders/lighting.glsl
+++ engine/src/shaders/lighting.glsl
@@ -1,6 +1,6 @@
-uniform vec4 sky_light_color;
-uniform vec3 sky_light_direction;
-uniform float ambient_light;
+uniform vec4 sky_light_color = vec4(1.0, 1.0, 1.0, 1.0);
+uniform vec3 sky_light_direction = normalize(vec3(-1.0, -1.0, -1.0));
+uniform float ambient_light = 0.5;
 
 vec4 apply_sky_lighting(vec4 color, vec3 normal, vec3 position) {
     float diffuse = max(dot(normal, sky_light_direction), 0.0);
