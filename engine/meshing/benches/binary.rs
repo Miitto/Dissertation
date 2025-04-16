f9cdae4df33c20b00f87a4e6641c37d00c1202f8
Miitto <67914241+Miitto@users.noreply.github.com>
2025-04-16 01:59:43 +0100

    feat: Binary algorithm benchmarks

--- engine/meshing/benches/binary.rs
+++ engine/meshing/benches/binary.rs
@@ -2,11 +2,16 @@ use std::{cell::RefCell, hint::black_box};
 
 use common::{
     Args, BlockType, seperate_global_pos,
-    tests::{Scene, test_scene},
+    tests::{Scene, Test, test_scene},
 };
 use criterion::{Criterion, criterion_group, criterion_main};
 
-use meshing::binary::{common::*, culled::Chunk};
+use dashmap::{DashMap, iter::Iter};
+use glam::IVec3;
+use meshing::binary::{
+    common::*,
+    culled::{Chunk, chunk_data, mesh_chunks},
+};
 
 pub fn culled(c: &mut Criterion) {
     let empty_fn = |_x: isize, _y: isize, _z: isize| -> BlockType { BlockType::Air };
@@ -23,33 +28,32 @@ pub fn culled(c: &mut Criterion) {
         })
     });
 
-    let args = Args::default();
+    let mut args = Args::default();
+    args.scene = Scene::Perlin;
+    args.test = Test::Culled;
 
     let blocks = test_scene(&args);
-    let mut chunks = std::collections::HashMap::new();
+    let chunks = DashMap::new();
 
-    blocks.iter().for_each(|r| {
-        let pos = r.key();
-        let block = r.value();
-        let (chunk_pos, in_chunk_pos) = seperate_global_pos(pos);
-
-        let chunk = chunks.entry(chunk_pos).or_insert(RefCell::new(Chunk::fill(
-            BlockType::Air,
-            false,
-            args.frustum_cull,
-        )));
-
-        chunk.borrow_mut().set(in_chunk_pos, *block);
-    });
+    chunk_data(&blocks, &args, &chunks);
 
     let mut chunk_iter = chunks.iter();
-    let (mut pos, mut chunk) = chunk_iter.next().unwrap();
 
-    let perlin_fn = |x: isize, y: isize, z: isize| -> BlockType {
-        chunk.borrow().get_at(x, y, z, pos, &chunks)
+    let mut next_item = || {
+        if let Some(e) = chunk_iter.next() {
+            e
+        } else {
+            chunk_iter = chunks.iter();
+            chunk_iter.next().unwrap()
+        }
     };
 
     c.bench_function("Get fn", |b| {
+        let e = next_item();
+        let pos = e.key();
+        let chunk = e.value();
+        let perlin_fn =
+            |x: isize, y: isize, z: isize| -> BlockType { chunk.get_at(x, y, z, pos, &chunks) };
         b.iter(|| {
             for x in -1..=32 {
                 for y in -1..=32 {
@@ -62,19 +66,50 @@ pub fn culled(c: &mut Criterion) {
     });
 
     c.bench_function("Perlin", |b| {
+        let e = next_item();
+        let pos = e.key();
+        let chunk = e.value();
+
+        let perlin_fn =
+            |x: isize, y: isize, z: isize| -> BlockType { chunk.get_at(x, y, z, pos, &chunks) };
         b.iter(|| {
             make_culled_faces(black_box(perlin_fn));
-            if let Some(next) = chunk_iter.next() {
-                pos = next.0;
-                chunk = next.1;
-            } else {
-                chunk_iter = chunks.iter();
-                let (next_pos, next_chunk) = chunk_iter.next().unwrap();
-                pos = next_pos;
-                chunk = next_chunk;
-            }
         })
     });
+
+    c.bench_function("Blocks to Chunks", |b| {
+        b.iter(|| {
+            let chunks = DashMap::new();
+            chunk_data(&blocks, &args, &chunks);
+        });
+    });
+
+    macro_rules! culled_bench {
+        ($radius:literal) => {
+            let chunks = DashMap::new();
+            args.scene = Scene::Perlin;
+            args.test = Test::Culled;
+            args.radius = $radius;
+            chunk_data(&blocks, &args, &chunks);
+
+            c.bench_function(stringify!(Parallel Culled $radius), |b| {
+                b.iter(|| {
+                    for chunk in chunks.iter() {
+                        let chunk = chunk.value();
+                        chunk.invalidate();
+                    }
+                    mesh_chunks(&chunks);
+                });
+            });
+        };
+    }
+
+    culled_bench!(32);
+    culled_bench!(64);
+    culled_bench!(128);
+    culled_bench!(256);
+    culled_bench!(512);
+    culled_bench!(1024);
 }
 
 criterion_group!(benches, culled);
